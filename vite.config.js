654d210dfa4280be6ee335aab2846b678c4e390e
Alfnrngg <alfin.rangga2004@gmail.com>
2026-04-23 16:34:47 +0700

    update: fix config and update project content

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // Tambahkan ini
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    react(), // Tambahkan ini supaya React-nya jalan
+    tailwindcss()
+  ],
 })
